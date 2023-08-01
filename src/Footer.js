ac4e3ed3e6fdf22232d6e0440c515e312edd328c
pnchoti <jgchotirat@gmail.com>
2023-08-01 14:16:43 +0200

    Create Footer.js

---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./index.css"
+
+export default function Footer() {
+    return (<div className="Footer">
+        <a href="https://github.com/jgchoti/weather-react" target="_blank" rel="noreferrer" > Open - source code </a>
+        , by
+        <a href="https://choti-portfolio.netlify.app/" target="_blank" rel="noreferrer" >
+            <img src="./src/logo_choti.png" alt="logo" /></a></div>
+    );
+}
