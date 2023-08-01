75d732dc54c06440f74e4a22bdc14d7ae0081737
pnchoti <jgchotirat@gmail.com>
2023-08-01 15:47:14 +0200

    Update Footer.js

--- src/Footer.js
+++ src/Footer.js
@@ -3,7 +3,7 @@ import "./index.css"
 
 export default function Footer() {
     return (<div className="Footer">
-        <a href="https://github.com/jgchoti/weather-react" target="_blank" rel="noreferrer" > Open - source code </a>
+        <a href="https://github.com/jgchoti/weather-react" target="_blank" rel="noreferrer" > Open-Source Code </a>
         , by <a href="https://choti-portfolio.netlify.app/" target="_blank" rel="noreferrer" >
             CHOTI</a></div>
     );
