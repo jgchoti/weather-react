5165c15574fb0d1a8a4ff180cab00940b3d59a8e
pnchoti <jgchotirat@gmail.com>
2023-08-03 11:09:39 +0200

    Update Footer.js

--- src/Footer.js
+++ src/Footer.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./index.css"
+import ChotiLogo from "./logo_choti.png"
 
 export default function Footer() {
     return (<div className="Footer">
         <a href="https://github.com/jgchoti/weather-react" target="_blank" rel="noreferrer" > Open-Source Code </a>
         , by <a href="https://choti-portfolio.netlify.app/" target="_blank" rel="noreferrer" >
-            CHOTI</a></div>
+            <img src={ChotiLogo} alt="logo" className="logo" /></a></div>
     );
 }
