dc20872705285acfe0b0995dbd6623d3b2ad52fa
pnchoti <jgchotirat@gmail.com>
2023-08-08 10:19:23 +0200

    Update Temperature.js

--- src/Temperature.js
+++ src/Temperature.js
@@ -1,28 +1,32 @@
 import React, { useState } from "react";
 
-
-export default function Unit(props) {
+export default function Temperature(props) {
     const [activeUnit, setActiveUnit] = useState("°C");
     const [inactiveUnit, setInactiveUnit] = useState("°F");
-    const [temperature, setTemperature] = useState(props.data)
+    const [temperature, setTemperature] = useState(props.data);
 
     function unitChange(event) {
         event.preventDefault();
         if (activeUnit === "°C") {
-            setTemperature(Math.round(props.data * 9 / 5) + 32)
+            const temperatureF = Math.round(props.data * 9 / 5) + 32;
+            setTemperature(temperatureF);
             setActiveUnit("°F");
             setInactiveUnit("°C");
         } else {
-            setTemperature(props.data)
+            setTemperature(props.data);
             setActiveUnit("°C");
             setInactiveUnit("°F");
         }
     }
 
-
-    return <div className="Temperature"><span>{temperature}</span>
-        <span className="ActiveUnit">{activeUnit}</span>
-        <span className="Divider">|</span>
-        <a href="/" onClick={unitChange} className="InactiveUnit">{inactiveUnit}</a>
-    </div>
+    return (
+        <div className="Temperature">
+            <span>{temperature}</span>
+            <span className="ActiveUnit">{activeUnit}</span>
+            <span className="Divider">|</span>
+            <a href="/" onClick={unitChange} className="InactiveUnit">
+                {inactiveUnit}
+            </a>
+        </div>
+    );
 }
