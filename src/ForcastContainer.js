e9dc0f0e225ff067138102ca1ac6dad8627eea90
pnchoti <jgchotirat@gmail.com>
2023-08-09 11:33:21 +0200

    Update ForcastContainer.js

--- src/ForcastContainer.js
+++ src/ForcastContainer.js
@@ -13,28 +13,8 @@ export default function ForcastContainer(props) {
   }
 
   function handleResponse(response) {
-    const groupedData = {};
-    response.data.list.forEach(function (dayData) {
-      const date = dayData.dt_txt.split(" ")[0];
-      if (!groupedData[date]) {
-        groupedData[date] = {
-          maxTemp: dayData.main.temp_max,
-          minTemp: dayData.main.temp_min,
-          icon: dayData.weather[0].icon,
-        };
-      } else {
-        if (dayData.main.temp_max > groupedData[date].maxTemp) {
-          groupedData[date].maxTemp = dayData.main.temp_max;
-        }
-        if (dayData.main.temp_min < groupedData[date].minTemp) {
-          groupedData[date].minTemp = dayData.main.temp_min;
-        }
-      }
-    });
-
-    const forecastData = Object.values(groupedData).slice(0, 6);
-    console.log(forecastData);
-    setForecast(forecastData);
+    console.log(response.data.daily);
+    setForecast(response.data.daily);
   }
 
   useEffect(() => {
@@ -44,7 +24,7 @@ export default function ForcastContainer(props) {
   function fetchForecastData() {
     const lon = props.data.lon;
     const lat = props.data.lat;
-    const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    const apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
     axios.get(apiUrl).then(handleResponse).catch(handleError);
   }
 
