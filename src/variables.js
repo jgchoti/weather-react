f225ae1bf92a8bad19ad616bb2eaefda9a3d66b9
pnchoti <jgchotirat@gmail.com>
2023-08-09 11:33:23 +0200

    Update variables.js

--- src/variables.js
+++ src/variables.js
@@ -1,3 +1,3 @@
 
-export const API_KEY = "0771d4347f81db89f7bfdf565868d867" // 1st api key (jgchotirat@gmail.com)
-// export const API_KEY = "0fff9337fdfe63e58cda1c8ffdd2c482" // 2nd api key (jgchotirat+weather@gmail.com)
+export const API_KEY = "c8a77112b2faf6684bb4b21a0aa778ae"
+// export const API_KEY = "0fff9337fdfe63e58cda1c8ffdd2c482" 
