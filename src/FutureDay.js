c40240f89c696311b3a2294c8cf3da9d68a6fab8
pnchoti <jgchotirat@gmail.com>
2023-08-10 12:02:53 +0200

    Update FutureDay.js

--- src/FutureDay.js
+++ src/FutureDay.js
@@ -1,18 +1,20 @@
 import React from "react";
-import "./ForcastContainer.css"
+import "./ForcastContainer.css";
 
 export default function FutureDay(props) {
     const date = new Date(props.currentDate);
-    let day = date.getDay()
-    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    const day = date.getDay();
+    const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
-    function generateDayElement(props, index) {
+    const dayElements = [];
+    for (let index = 0; index < 5; index++) {
         const dayIndex = (day + index + 1) % 7;
-        return <div className="FutureDay col" key={dayIndex}>{days[dayIndex]}</div>;
+        dayElements.push(
+            <div className="FutureDay col" key={dayIndex}>
+                {days[dayIndex]}
+            </div>
+        );
     }
 
-    const dayElements = Array.from({ length: 5 }).map(generateDayElement);
     return <div className="row">{dayElements}</div>;
 }
-
-
